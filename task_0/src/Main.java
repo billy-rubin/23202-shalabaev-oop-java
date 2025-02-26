78aa67126c027aabf2e12ae64a1a6d48d9ab7c38
billy-rubin <143946311+billy-rubin@users.noreply.github.com>
2025-02-26 09:30:21 +0700

    Delete task_0/src directory

--- task_0/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        Game game = new Game(5, 4);
-        //System.out.println(game.getAnswer());
-        game.runGame();
-    }
-}
